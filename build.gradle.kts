8c8fbfa95978bcc6d627fe26a806eb810282f9be
Jason Monk <monkopedia@gmail.com>
2026-04-05 15:38:40 -0400

    Add Firebase: google-services.json, Google Services plugin, debug applicationIdSuffix

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,7 @@ plugins {
     alias(libs.plugins.ktlint) apply false
     alias(libs.plugins.detekt)
     alias(libs.plugins.roborazzi) apply false
+    alias(libs.plugins.google.services) apply false
 }
 
 subprojects {
